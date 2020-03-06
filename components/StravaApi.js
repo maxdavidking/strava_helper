2574d116f8092b53ebb8a18e4dab6eb103bfe149
maxdavidking <maxdavidking@gmail.com>
2020-03-06 12:18:43 -0500

    Get rid of default next js content and add styled-components and strava component

---
+++ components/StravaApi.js
@@ -0,0 +1,10 @@
+import { useState, useEffect } from 'react';
+
+const StravaApi = () => {
+  const [apiUrl, setApiUrl] = useState('');
+  return (
+    <div> Let's get your Strava Account! </div>
+  );
+};
+
+export default StravaApi;
