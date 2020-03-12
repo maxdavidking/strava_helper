ba45340e0a8083170c6be6c3a3d6572482bcd37c
maxdavidking <maxdavidking@gmail.com>
2020-03-12 15:53:35 -0400

    All requests working.... sort of

--- components/StravaApi.jsx
+++ components/StravaApi.jsx
@@ -1,79 +1,95 @@
 import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import axios from 'axios';
 
 const StravaApi = () => {
-  const [stravaId, setStravaId] = useState('');
+  const [stravaId, setStravaId] = useState('Unefined');
+  const [stravaCode, setStravaCode] = useState('');
   const [accessToken, setAccessToken] = useState('');
+  const [refreshToken, setRefreshToken] = useState(1);
   const [userRunCount, setUserRunCount] = useState('Loading....');
   const [lastActivity, setLastActivity] = useState('Loading...');
+  const { query: { code } } = useRouter();
 
   useEffect(() => {
     const refreshUrl = 'https://www.strava.com/api/v3/oauth/token';
     const baseUrl = 'https://www.strava.com/api/v3';
-    const athleteUrl = '/athlete';
     const athleteStatsUrl = `/athletes/${stravaId}/stats`;
-    const lastActivityUrl = `/athletes/${stravaId}/activities`;
+    const allActivitiesUrl = '/athlete/activities';
+    setStravaCode(code);
 
+    // Fix the conditional logic to post this
+    if (!refreshToken && !accessToken && !stravaId) {
+      axios
+        .post('https://www.strava.com/api/v3/oauth/token', {
+          client_id: '44378',
+          client_secret: '68d0196721ef3be5a5907b8436ed965bd35e2a36',
+          grant_type: 'authorization_code',
+          code: stravaCode
+        })
+        // Add success handling. Need to update the access token from the response.
+        .then((response) => {
+          setRefreshToken(response.data.access_token);
+          setAccessToken(response.data.access_token);
+          setStravaId(response.data.athlete.id);
+        })
+        // Add error handling. If there is no response this app won't work
+        .catch((error) => {
+          console.log('oauth post error', error);
+        });
+    }
     // Need to make a request to Strava to check that the access token hasn't
     // expired. Expires every 6 hours. Need the client_id, client_secret and
     // refresh token, which are unique to each person.
-    axios
-      .post(`${refreshUrl}`, {
-        client_id: '44378',
-        client_secret: '68d0196721ef3be5a5907b8436ed965bd35e2a36',
-        grant_type: 'refresh_token',
-        refresh_token: '5a5936fa97fe9240dec43f002d0b1561f8315425'
-      })
-      // Add success handling. Need to update the access token from the response.
-      .then((response) => {
-        setAccessToken(response.data.access_token);
-      })
-      // Add error handling. If there is no response this app won't work
-      .catch(() => {
-        setAccessToken('');
-      });
-
-    if (accessToken) {
+    // TODO check if this actually checks if refreshToken is expired and fix the
+    // hack of setting refreshToken default to 1
+    if (refreshToken <= 0) {
+      console.log('hello from refresh token');
       axios
-        .get(`${baseUrl}${athleteUrl}`, {
-          headers: { Authorization: `Bearer ${accessToken}` }
+        .post(`${refreshUrl}`, {
+          client_id: '44378',
+          client_secret: '68d0196721ef3be5a5907b8436ed965bd35e2a36',
+          grant_type: 'refresh_token',
+          refresh_token: refreshToken
         })
+        // Add success handling. Need to update the access token from the response.
         .then((response) => {
-          const { id } = response.data;
-          setStravaId(id);
+          setAccessToken(response.data.access_token);
         })
+        // Add error handling. If there is no response this app won't work
         .catch(() => {
-          setStravaId('');
+          setAccessToken('');
         });
     }
 
     if (stravaId) {
+      // get Athlete
       axios
         .get(`${baseUrl}${athleteStatsUrl}`, {
           headers: { Authorization: `Bearer ${accessToken}` }
         })
         .then((response) => {
-          const { data: { recent_run_totals: count } } = response;
+          const { data: { recent_run_totals: { count } } } = response;
           setUserRunCount(count);
         })
         .catch(() => {
           setUserRunCount('Error!');
         });
-      // TODO: Need to fix activities search
+      // get Athlete activities
       axios
-        .get(`${baseUrl}${lastActivityUrl}`, {
+        .get(`${baseUrl}${allActivitiesUrl}?per_page=30`, {
           headers: { Authorization: `Bearer ${accessToken}` }
         })
         .then((response) => {
           const { data } = response;
-          console.log(response);
-          setLastActivity(data);
+          setLastActivity(data[0].resource_state);
         })
         .catch(() => {
           setLastActivity('Error!');
         });
     }
-  }, [stravaId, accessToken]);
+  }, [accessToken, refreshToken, stravaCode, stravaId, code]);
+
   return (
     <div>
       <div>
@@ -84,6 +100,9 @@ const StravaApi = () => {
         Your last activity is:
         {lastActivity}
       </div>
+      <a href="https://www.strava.com/oauth/authorize?client_id=44378&redirect_uri=http://localhost:3003&response_type=code&scope=activity:read_all">
+        Authorize!
+      </a>
     </div>
   );
 };
--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,6 @@ const Home = () => {
   const Grid = styled.div`
     display: grid;
   `;
-
   return (
     <>
       <Head>
