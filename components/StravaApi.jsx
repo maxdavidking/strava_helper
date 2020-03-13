c4c37f0479ac97582cece9f5ba396c74b120f639
maxdavidking <maxdavidking@gmail.com>
2020-03-13 14:11:18 -0400

    Clean up code and render out list activities

--- components/StravaApi.jsx
+++ components/StravaApi.jsx
@@ -3,48 +3,54 @@ import { useRouter } from 'next/router';
 import axios from 'axios';
 
 const StravaApi = () => {
-  const [stravaId, setStravaId] = useState('Unefined');
-  const [stravaCode, setStravaCode] = useState('');
+  // TODO redirect to base URL once the code from Strava is captured
+  const [stravaUserId, setStravaUserId] = useState('');
+  const [stravaOauthCode, setStravaOauthCode] = useState('');
   const [accessToken, setAccessToken] = useState('');
   const [refreshToken, setRefreshToken] = useState(1);
-  const [userRunCount, setUserRunCount] = useState('Loading....');
-  const [lastActivity, setLastActivity] = useState('Loading...');
-  const { query: { code } } = useRouter();
+  const [userRunCount, setUserRunCount] = useState(false);
+  const [userActivities, setUserActivities] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+
+  const {
+    query: { code }
+  } = useRouter();
+
+  const refreshUrl = 'https://www.strava.com/api/v3/oauth/token';
+  const baseUrl = 'https://www.strava.com/api/v3';
+  const allActivitiesUrl = '/athlete/activities';
+  const clientId = '44378';
+  const clientSecret = '68d0196721ef3be5a5907b8436ed965bd35e2a36';
 
   useEffect(() => {
-    const refreshUrl = 'https://www.strava.com/api/v3/oauth/token';
-    const baseUrl = 'https://www.strava.com/api/v3';
-    const athleteStatsUrl = `/athletes/${stravaId}/stats`;
-    const allActivitiesUrl = '/athlete/activities';
-    setStravaCode(code);
+    setStravaOauthCode(code);
+    axios
+      .post('https://www.strava.com/api/v3/oauth/token', {
+        client_id: clientId,
+        client_secret: clientSecret,
+        grant_type: 'authorization_code',
+        code: stravaOauthCode
+      })
+      .then((response) => {
+        setRefreshToken(response.data.refresh_token);
+        setAccessToken(response.data.access_token);
+        setStravaUserId(response.data.athlete.id);
+      })
+      // Add error handling. If there is no response this app won't work
+      .catch(() => {
+        setStravaUserId(false);
+      });
+  }, [code, stravaOauthCode]);
 
-    // Fix the conditional logic to post this
-    if (!refreshToken && !accessToken && !stravaId) {
-      axios
-        .post('https://www.strava.com/api/v3/oauth/token', {
-          client_id: '44378',
-          client_secret: '68d0196721ef3be5a5907b8436ed965bd35e2a36',
-          grant_type: 'authorization_code',
-          code: stravaCode
-        })
-        // Add success handling. Need to update the access token from the response.
-        .then((response) => {
-          setRefreshToken(response.data.access_token);
-          setAccessToken(response.data.access_token);
-          setStravaId(response.data.athlete.id);
-        })
-        // Add error handling. If there is no response this app won't work
-        .catch((error) => {
-          console.log('oauth post error', error);
-        });
-    }
+  // TODO put into it's own component?
+  useEffect(() => {
     // Need to make a request to Strava to check that the access token hasn't
-    // expired. Expires every 6 hours. Need the client_id, client_secret and
-    // refresh token, which are unique to each person.
+    // expired. Expires every 6 hours. Need the client_id, client_secret, which is
+    // unique to the app and the refresh token, which is unique to each person.
     // TODO check if this actually checks if refreshToken is expired and fix the
     // hack of setting refreshToken default to 1
     if (refreshToken <= 0) {
-      console.log('hello from refresh token');
       axios
         .post(`${refreshUrl}`, {
           client_id: '44378',
@@ -61,19 +67,27 @@ const StravaApi = () => {
           setAccessToken('');
         });
     }
+  }, [refreshToken]);
 
-    if (stravaId) {
+  // TODO put into it's own component?
+  useEffect(() => {
+    if (stravaUserId) {
+      const athleteStatsUrl = `/athletes/${stravaUserId}/stats`;
       // get Athlete
       axios
         .get(`${baseUrl}${athleteStatsUrl}`, {
           headers: { Authorization: `Bearer ${accessToken}` }
         })
         .then((response) => {
-          const { data: { recent_run_totals: { count } } } = response;
+          const {
+            data: {
+              recent_run_totals: { count }
+            }
+          } = response;
           setUserRunCount(count);
         })
         .catch(() => {
-          setUserRunCount('Error!');
+          setUserRunCount(false);
         });
       // get Athlete activities
       axios
@@ -82,23 +96,42 @@ const StravaApi = () => {
         })
         .then((response) => {
           const { data } = response;
-          setLastActivity(data[0].resource_state);
+          setUserActivities(data);
         })
         .catch(() => {
-          setLastActivity('Error!');
+          setUserActivities(false);
         });
     }
-  }, [accessToken, refreshToken, stravaCode, stravaId, code]);
+  }, [accessToken, refreshToken, stravaUserId]);
+
+  // TODO turn these into loading/error components
+  if (isLoading) {
+    return <div> Loading.... </div>;
+  }
+
+  if (hasError) {
+    return <div> Error! </div>;
+  }
 
   return (
     <div>
       <div>
-        Your strava id is:
-        {stravaId}
-        Your run count is:
-        {userRunCount}
-        Your last activity is:
-        {lastActivity}
+        <h2> Your strava id is:</h2>
+        <p>{stravaUserId}</p>
+        <h2> Your run count is:</h2>
+        <p>{userRunCount && 'No Data!'}</p>
+        <h2> Your Activities:</h2>
+        <div>
+          {userActivities
+            ? userActivities.map((activity) => (
+              <>
+                <div>{`Name: ${activity.name}`}</div>
+                <div>{`Distance: ${activity.distance}`}</div>
+                <div>{`Time: ${activity.moving_time}`}</div>
+              </>
+            ))
+            : 'No acitivites'}
+        </div>
       </div>
       <a href="https://www.strava.com/oauth/authorize?client_id=44378&redirect_uri=http://localhost:3003&response_type=code&scope=activity:read_all">
         Authorize!
